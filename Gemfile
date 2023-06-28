767811c5b51516b3bf9def4a926ddd85dda821fe
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-06-28 15:06:17 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in gravatar_helper.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/gravatar_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gravatar_helper/version"
+
+module GravatarHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
